43549ff43b244ad054471e6205e352e7af6315ca
Martina Kamal <tt034232@gmail.com>
2022-06-27 15:53:36 +0200

    solving errors showen after questions finished

--- test/lib/main.dart
+++ test/lib/main.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: deprecated_member_use, prefer_const_constructors, duplicate_ignore, avoid_print
-import 'dart:convert';
 
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import "./question.dart";
 import './answer.dart';
@@ -11,6 +11,8 @@ import './answer.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   State<MyApp> createState() {
     return _MyAppState();
@@ -18,45 +20,55 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final questions = const [
+    {
+      'questionText': "what is your fav color? ",
+      'answers': ['Red', 'Orange', 'Green', 'Blue'],
+    },
+    {
+      'questionText': "what's your name?",
+      'answers': ['Martina', 'Mina', 'Micheal', 'Mano'],
+    },
+    {
+      'questionText': "what's fav animal?",
+      'answers': ['Cat', 'Dog', 'Rabbit', 'Monkey'],
+    }
+  ];
   var _questionIndex = 0;
-
   void _answerQuestion() {
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
     print(_questionIndex);
+    if (_questionIndex < questions.length) {
+      print("we have more questions");
+    } else {
+      print(" No more question ");
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     // ignore: unused_local_variable
-    var questions = [
-      {
-        'questionText': "what is your fav color? ",
-        'answer': ['Red', 'Orange', 'Green', 'Blue'],
-      },
-      {
-        'questionText': "what's your name?",
-        'answer': ['Martina', 'Mina', 'Micheal', 'Mano'],
-      },
-      {
-        'questionText': "what's fav animal?",
-        'answer': ['Cat', 'Dog', 'Rabbit', 'Monkey'],
-      }
-    ];
 
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(title: Text("Home Page")),
-      body: Column(
-        children: [
-          Question(jsonEncode(questions[_questionIndex]['questionText'])),
-          ...(questions[_questionIndex]['answers'] as List<String>)
-              .map((answer) {
-            return Answer(_answerQuestion, answer);
-          }).toList()
-        ],
-      ),
+      body: _questionIndex < questions.length
+          ? Column(
+              children: [
+                Question(
+                  jsonEncode(questions[_questionIndex]["questionText"]),
+                ),
+                ...(questions[_questionIndex]["answers"] as List<String>)
+                    .map((answers) {
+                  return Answer(_answerQuestion, answers);
+                }).toList()
+              ],
+            )
+          : Center(
+              child: Text("you did it !"),
+            ),
     ));
   }
 }
